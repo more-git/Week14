b57cb671ba680bc9f71fb3751c6c6a41e8f4005f
Nick Minnella <nickminnella@gmail.com>
2020-11-26 16:25:59 -0600

    Add files via upload

---
+++ src/app/_services/names.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { environment } from "../../environments/environment";
+import {Person} from "../_models/person.model";
+import {map} from "rxjs/operators";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NamesService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  getAll(): Observable<any> {
+    return this.httpClient.get<any>(`${environment.apiUrl}/names`)
+  }
+
+  create( newName ): Observable<Person> {
+    return this.httpClient.post<any>(`${environment.apiUrl}/name`, {name: newName})
+      .pipe(map(newName => new Person(newName.name)));
+  }
+ 
+  destroy( name ): Observable<any> {
+    return this.httpClient.delete(`${environment.apiUrl}/name/${name._id}`)
+  }
+
+  removeName( arrayNames, nameToRemove ): any{
+    for (let i = 0; i <= arrayNames.length; i++) {
+      if (arrayNames[i]._id === nameToRemove._id) {
+        arrayNames.splice(i, 1);
+        return arrayNames;
+      }
+    }
+  }
+
+  updateName( nameToUpdate ): any{
+    let options = { name: nameToUpdate.name, _id: nameToUpdate._id };
+    return this.httpClient.request<any>('PUT', `${environment.apiUrl}/update/`, { body: options });
+    //return this.httpClient.request<any>('PUT', `${environment.apiUrl}/update/${nameToUpdate._id}/${nameToUpdate.name}`);
+  }
+
+  editName( arrayNames, nameToEdit ): any{
+    for (let i = 0; i <= arrayNames.length; i++) {
+      if (arrayNames[i]._id === nameToEdit._id) {
+        arrayNames[i].name = nameToEdit.name;
+        return arrayNames;
+      }
+    }
+  }
+}
